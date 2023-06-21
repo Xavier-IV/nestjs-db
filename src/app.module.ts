06de5d172c8ba2b8c2caea984587b566fe5a2f8a
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2023-06-21 17:30:34 +0200

    Include entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { User } from './user/user.entity';
 
 @Module({
   imports: [
@@ -13,7 +14,7 @@ import { UserModule } from './user/user.module';
       username: 'root',
       password: 'password',
       database: 'database_1',
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
     UserModule,
