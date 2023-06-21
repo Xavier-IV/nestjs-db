33a1f422a7d255867077e9893e33eda6d114bb80
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2023-06-21 16:13:35 +0200

    Integrate controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -8,5 +17,25 @@ export class UserController {
   @Get()
   index(): any[] {
     return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number): number {
+    return this.userService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() body): number {
+    return this.userService.create(body);
+  }
+
+  @Put(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() body) {
+    return this.userService.update(id, body);
+  }
+
+  @Delete(':id')
+  destroy(@Param('id', ParseIntPipe) id: number): boolean {
+    return this.userService.destroy(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,4 +5,20 @@ export class UserService {
   findAll() {
     return [];
   }
+
+  findOne(id: number) {
+    return id;
+  }
+
+  create(body) {
+    return body;
+  }
+
+  update(id, body) {
+    return body;
+  }
+
+  destroy(id: number) {
+    return true;
+  }
 }
